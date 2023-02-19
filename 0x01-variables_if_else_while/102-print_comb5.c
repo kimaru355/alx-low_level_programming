d386d14bd3cdf46019ac3c529e0a6fe62e4cadc1
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-19 05:45:16 -0800

    start

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,46 +8,24 @@
 
 int main(void)
 {
-	int a, b, c, d;
+	int a, b;
 
-	for (a = 0; a < 10; a++)
+	for (a = 0; a < 100; a++)
 	{
-		for (b = 0; b < 10; b++)
+		for (b = 0; b < 100; b++)
 		{
-			for (c = 0; c < 10; c++)
-			{
-				for (d = 0; d < 10; d++)
-				{
-					if ((a + b) <= (c + d))
-					{
-					if (!((a == c) && (b == d)))
-					if ((a == d) && (b == c))
-					{
-					if ((a + b) == (c + d))
-					{
-					continue;
-					}
-					}
-					{
-					putchar((a % 10) + '0');
-					putchar((b % 10) + '0');
-					putchar(32);
-					putchar((c % 10) + '0');
-					putchar((d % 10) + '0');
-					if (!((a == 9) && (b == 8)))
-					{
-					putchar(44);
-					putchar(32);
-					}
-					}
-					}
-					else
-						continue;
-				}
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(32);
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+			if ((a == 98) && (b == 99))
+				continue;
+			putchar(44);
+			putchar(32);
 			}
 		}
 	}
-
 	putchar('\n');
 
 	return (0);
