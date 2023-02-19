2be70754449a732292081cfa0fc9d90a68099bed
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-19 02:42:52 -0800

    done

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,16 +9,12 @@
 int main(void)
 {
 	int i;
-	int a;
 
 	for (i = 0; i < 10; i++)
-	{
-		for (a = 0; a < 10; a++)
-		{
-			putchar((i % 10) + '0');
-			putchar((a % 10) + '0');
-			putchar(44);
-		}
+	{	
+		putchar((i % 10) + '0');
+		putchar(44);
+		putchar(32);
 	}
 	putchar('\n');
 
