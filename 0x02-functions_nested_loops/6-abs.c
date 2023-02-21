6552926c7ada1ef62a4c2730b5043bcb2494a7d0
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-21 06:53:52 -0800

    done

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - computes abolute value
+ *
+ * @n: return value
+ *
+ * Return: 0
+ */
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else if (n < 0)
+		return (-n);
+	else
+		return (n);
+
+}
