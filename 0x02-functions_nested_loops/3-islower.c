813b0d66269fb6246281505bf0665d51a29dfc3d
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-21 05:27:41 -0800

    start

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase charater
+ *
+ * @c: - aphabet
+ *
+ * Return: 1 if lowercase and 0 otherwise
+ */
+
+int _islower(int c)
+{
+	if ((c >= 'a') && (c <= 'z'))
+		return (1);
+	else
+		return (0);
+}
