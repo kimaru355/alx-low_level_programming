16ec87c47a8f296b3da92b512d14b2ef76d0b519
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-21 11:46:37 -0800

    done

---
+++ 0x02-functions_nested_loops/100-main.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - check code
+ * Description: prints the n times table
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	print_times_table(3);
+	_putchar('\n');
+	print_times_table(5);
+	_putchar('\n');
+	print_times_table(98);
+	_putchar('\n');
+	print_times_table(12);
+	_putchar('\n');
+	return (0);
+}
+
