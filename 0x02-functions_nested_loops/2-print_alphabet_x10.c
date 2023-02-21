9486bd6b0e8fb7d850f30951b162a1e058f9eae9
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-21 03:10:54 -0800

    pause

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,5 @@ void print_alphabet_x10(void)
 {
 	char ch;
 
-	for(ch = 'a'; ch <='z'; ch++)
-	{
-		_putchar(ch);
-		_putchar('\n');
-	}
+
 }
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
+
+#endif
