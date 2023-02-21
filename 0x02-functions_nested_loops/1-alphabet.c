f69f72cbfe64cd46b6172f43f70ce39cb3c499ab
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-21 02:23:18 -0800

    done

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,17 @@
+#include <stdio.h>
 #include "main.h"
 
-/** main - calls custom header
+/**
+ *print_alphabet - prints alphabets in lowercase
  *
  * Return: Always 0.
  */
 
-int main(
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	putchar('\n');
+}
--- 0x02-functions_nested_loops/1-main.c
+++ 0x02-functions_nested_loops/1-main.c
@@ -8,7 +8,7 @@
 
 int main(void)
 {
-	print_aLphabet();
+	print_alphabet();
 
 	return (0);
 }
