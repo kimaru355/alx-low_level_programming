8311697a4778278cdef1dcc265a1947a79d396cd
kimaru355 <emmanuelkimaru355@gmail.com>
2023-02-23 11:48:51 -0800

    done

---
+++ 0x04-more_functions_nested_loops/10-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * main - check the code
+ * Description: prints a triangle, followed by a new line
+ * Return: Always 0(Success)
+ */
+int main(void)
+{
+	print_triangle(2);
+	print_triangle(10);
+	print_triangle(1);
+	print_triangle(0);
+	return (0);
+}
+
