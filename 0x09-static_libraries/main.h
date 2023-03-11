b0d17505bb64e9af1991fb64b2dd4489c9f6734b
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-11 06:03:06 -0800

    done

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef FILE_MAIN
+#define FILE_MAIN
 
 int _putchar(char c);
 int _islower(int c);
@@ -23,3 +23,4 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
 #endif
+
