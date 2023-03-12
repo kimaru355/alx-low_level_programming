df1036784c734ffee5cfaba55d69d852e1ac730b
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-12 04:11:30 -0700

    done

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints argument passed to main
+ *
+ * @argc: number of arguments
+ * @argv: arguments passed there
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[] __attribute__ ((unused)))
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
