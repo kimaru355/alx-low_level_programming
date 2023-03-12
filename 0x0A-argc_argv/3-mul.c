8246414acaa3bd7a2a13c38d9594dff269651687
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-12 05:14:45 -0700

    done

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - mul nums
+ *
+ * @argc: numargs
+ * @argv: args
+ *
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int res __attribute__ ((unused));
+
+	argc += -1;
+	if (argc > 0)
+	{
+		printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+
+	return (0);
+}
