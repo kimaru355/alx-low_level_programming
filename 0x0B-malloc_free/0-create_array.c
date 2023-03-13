868bb65f629d99d6269946b708b86e4626b54fc7
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-13 05:15:34 -0700

    start

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -16,7 +16,7 @@ char *create_array(unsigned int size, char c)
 
 	if (size == 0)
 		return (NULL);
-	arr = malloc(sizeof(size) * size);
+	arr = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 	{
 		arr[i] = c;
