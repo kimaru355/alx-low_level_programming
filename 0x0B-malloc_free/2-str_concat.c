43179cbb01add2c4b8153df5f2ca61e4d1b772c7
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-14 13:29:44 -0700

    malloc with arrays

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,52 +3,45 @@
 #include <stdio.h>
 
 /**
- * str_concat - join two strings
+ * str_concat - concatenates two strings.
  * @s1: first string
  * @s2: second string
- * Return: p to joined string
+ *
+ * Return: a pointer to a newly allocated space in memory which
+ * contains the contents of s1, followed by the contents of s2,
+ * and null terminated. NULL on failure
  */
-
 char *str_concat(char *s1, char *s2)
 {
-	char *tmp1, *tmp2;
-	char *arr;
-	unsigned int len1, len2, sum;
-	unsigned int j, k;
+	int i, j, len1, len2, len;
+	char *result;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
+	len1 = len2 = 0;
 
-	tmp1 = s1;
-	tmp2 = s2;
+	if (s1 != NULL)
+	{
+		i = 0;
+		while (s1[i++] != '\0')
+			len1++;
+	}
 
-	len1 = 0;
-	while (*s1++)
-		len1++;
-	len2 = 0;
-	while (*s2++)
-		len2++;
-	s1 = tmp1;
-	s2 = tmp2;
-	sum = len1 + len2;
+	if (s2 != NULL)
+	{
+		i = 0;
+		while (s2[i++] != '\0')
+			len2++;
+	}
 
-	arr = malloc(sizeof(char) * sum + 1);
-	if (arr == NULL)
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
 		return (NULL);
 
-	for (k = 0; k < len1; k++)
-	{
-		arr[k] = s1[k];
-	}
-	for (; k < sum; k++)
-	{
-		for (j = 0; j < len2; j++)
-		{
-			arr[k] = s2[j];
-		}
-	}
-	arr[k] = '\0';
-	return (arr);
+	for (i = 0; i < len1; i++)
+		result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+		result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
