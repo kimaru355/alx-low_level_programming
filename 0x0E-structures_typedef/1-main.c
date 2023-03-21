4dc2b57b838faf299947e8eb948615b027271e28
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-21 07:53:15 -0700

    exciting how we handle NULL

--- 0x0E-structures_typedef/1-main.c
+++ 0x0E-structures_typedef/1-main.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-    struct dog my_dog;
+    struct dog NULL;
 
-    init_dog(&my_dog, "Poppy", 3.5, "Bob");
-    printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog.name, my_dog.age);
+    init_dog(NULL, "Poppy", 3.5, "Bob");
+    printf("My name is %s, and I am %.1f :) - Woof!\n", NULL.name, NULL.age);
     return (0);
 }
