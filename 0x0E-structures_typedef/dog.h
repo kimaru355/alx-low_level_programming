fc338eb02468268097907a839cd7c0b853bef590
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-21 06:30:15 -0700

    add semicolon after creating structure, also comment structure for easier understanding

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,9 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - dog structure
+ * @name: name in structure
+ * @age: age of 'name'
+ * @owner: owner of structure
+ */
+
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}
+};
 #endif /* !DOG_H */
