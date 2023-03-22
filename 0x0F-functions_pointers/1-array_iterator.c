fc1e82eeb2cdb840ad743364aa9dfffae0e449fb
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-22 07:05:20 -0700

    whats not happening

--- 0x0F-functions_pointers/1-array_iterator.c
+++ 0x0F-functions_pointers/1-array_iterator.c
@@ -12,8 +12,11 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	int i;
 
-	for (i = 0; i < (int)size; i++)
+	if (array && size && action)
 	{
-		action(array[i]);
+		for (i = 0; i < (int)size; i++)
+		{
+			action(array[i]);
+		}
 	}
 }
