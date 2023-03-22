ce3db562d738233d489ebb91a2016323cbe2b6b5
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-22 08:06:12 -0700

    relaxed

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -10,7 +10,5 @@
 void print_name(char *name, void (*f)(char *))
 {
 	if (name && f)
-	{
 		f(name);
-	}
 }
--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -12,13 +12,12 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (array && cmp)
 	{
-		int i;
+		int i, ans;
 
-		if (size <= 0)
-			return (-1);
 		for (i = 0; i < size; i++)
 		{
-			if (cmp(array[i]))
+			ans = (cmp(array[i]));
+			if (ans != 0)
 				return (i);
 		}
 	}
