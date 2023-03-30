d2a90b9490957a97c26497c04a66977926b73765
kimaru355 <emmanuelkimaru355@gmail.com>
2023-03-30 09:07:28 -0700

    unknown error

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,11 +1,11 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <stding.h>
+#include <string.h>
 
-**
+/**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
  * @len: length of the string
@@ -14,7 +14,6 @@
  * Description: singly linked list node structure
  * for Holberton project
  */
-
 typedef struct list_s
 {
 	char *str;
@@ -29,3 +28,4 @@ list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
+
