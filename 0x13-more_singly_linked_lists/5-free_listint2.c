950d6a67ad38e7726fc55b3461cf697d90d4443d
kimaru355 <emmanuelkimaru355@gmail.com>
2023-04-03 15:51:21 -0700

    more singly linked lists for further understanding

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - frees a linked list
+ * @head: head of a list.
+ *
+ * Return: no return.
+ */
+void free_listint2(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *cur;
+
+	if (head != NULL)
+	{
+		cur = *head;
+		while ((temp = cur) != NULL)
+		{
+			cur = cur->next;
+			free(temp);
+		}
+		*head = NULL;
+	}
+}
+
